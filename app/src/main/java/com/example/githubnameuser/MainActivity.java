5a9575cd96e6467b0fec0a9df3de894056d71fcc
Филиппов Сергей <filippovsergej@iMac-Mac009.local>
2021-02-24 14:25:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/githubnameuser/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.githubnameuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button button;
+    mTextView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+
+        final Call<List<Repos>> call = gitHubService.getRepos("alexanderklimov");
+
+        call.enqueue(new Callback<List<Repos>>() {
+                         @Override
+                         public void onResponse(Call<List<Repos>> call, Response<List<Repos>> response) {
+                             // response.isSuccessfull() is true if the response code is 2xx
+                             if (response.isSuccessful()) {
+                                 // Выводим массив имён
+                                 mTextView.setText(response.body().toString() + "\n");
+                                 for (int i = 0; i < response.body().size(); i++) {
+                                     // Выводим имена по отдельности
+                                     mTextView.append(response.body().get(i).getName() + "\n");
+                                 }
+
+                                 mProgressBar.setVisibility(View.INVISIBLE);
+                             } else {
+                                 int statusCode = response.code();
+                                 // Обрабатываем ошибку
+                                 ResponseBody errorBody = response.errorBody();
+                                 try {
+                                     mTextView.setText(errorBody.string());
+                                     mProgressBar.setVisibility(View.INVISIBLE);
+                                 } catch (IOException e) {
+                                     e.printStackTrace();
+                                 }
+                             }
+                         }
+
+                         @Override
+                         public void onFailure(Call<List<Repos>> call, Throwable throwable) {
+                             mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+                         }
+                     }
+        );
+    }
+}
